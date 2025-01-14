64434b64c169f963e1a02734d75683e13957a096
Ainyaku <87048351+Ainyaku@users.noreply.github.com>
2025-01-13 20:10:28 -0500

    Update arch-mount.sh

--- cdn/arch-mount.sh
+++ cdn/arch-mount.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-# Command Syntax: curl https://experiments.ainyaku.com/cdn/arch-mount.sh | sh [Root partition name without /dev/]
+# Command Syntax: curl https://experiments.ainyaku.com/cdn/arch-mount.sh | sh
 
-echo Root partition: $1
-echo Boot partition: ${1::-1}1
+echo Detected partitions:
+lsblk
+echo
+echo Enter the name of the root partition \(without "/dev/"\):
+read part
 
-mount -o noatime,space_cache=v2,compress=zstd,subvol=@ /dev/$1 /mnt
+echo
+echo Root partition: $part
+echo Boot partition: ${part::-1}1
+
+echo
+mount -o noatime,space_cache=v2,compress=zstd,subvol=@ /dev/$part /mnt
 mkdir -p /mnt/{efi,home,swap}
-mount -o noatime,space_cache=v2,compress=zstd,subvol=@home /dev/$1 /mnt/home
-mount -o noatime,space_cache=v2,compress=zstd,subvol=@swap /dev/$1 /mnt/swap
-mount /dev/${1::-1}1 /mnt/efi
+mount -o noatime,space_cache=v2,compress=zstd,subvol=@home /dev/$part /mnt/home
+mount -o noatime,space_cache=v2,compress=zstd,subvol=@swap /dev/$part /mnt/swap
+mount /dev/${part::-1}1 /mnt/efi
