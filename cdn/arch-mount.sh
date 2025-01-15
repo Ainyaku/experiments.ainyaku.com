497ce50149cf9b27648620e6ed420df5a8c40b1a
Ainyaku <87048351+Ainyaku@users.noreply.github.com>
2025-01-15 16:31:09 -0500

    Update arch-mount.sh

--- cdn/arch-mount.sh
+++ cdn/arch-mount.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Command Syntax: curl -O https://experiments.ainyaku.com/cdn/arch-mount.sh && sh arch-mount.sh [Root partition name without /dev/]
+# Command Syntax: curl -O https://experiments.ainyaku.com/cdn/arch-mount.sh && sh arch-mount.sh [Root partition name without /dev/] [Boot partition name without /dev/]
 
 echo Root partition: $1
-echo Boot partition: ${1::-1}1
+echo Boot partition: $2
 
 mount -o noatime,space_cache=v2,compress=zstd,subvol=@ /dev/$1 /mnt
 mkdir -p /mnt/{efi,home,swap}
 mount -o noatime,space_cache=v2,compress=zstd,subvol=@home /dev/$1 /mnt/home
 mount -o noatime,space_cache=v2,compress=zstd,subvol=@swap /dev/$1 /mnt/swap
-mount /dev/${1::-1}1 /mnt/efi
+mount /dev/$2 /mnt/efi
